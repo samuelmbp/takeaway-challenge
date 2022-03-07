3ea6ba3d21e45b20e72c07ed7289eaaa2b778f1e
Samuel Raducan <samuel.thembp@gmail.com>
2022-03-07 18:36:19 +0000

    Update test for add to accept several dishes and quantities

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,5 +7,10 @@ describe Order do
     it 'responds to a add with two arguments' do
       expect(order).to respond_to(:add).with(2).arguments
     end
+    it 'selects some several available dishes' do
+      order.add(:lasagna, 2)
+      order.add(:burger, 1)
+      expect(order.dishes).to eq ({:lasagna => 2, :burger => 1})
+    end
   end
 end
